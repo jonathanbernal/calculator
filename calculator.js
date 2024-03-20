469ed9c7d3f5ab7f1c7e74e978233cb8f536228b
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2024-03-19 22:00:55 -0400

    Add values to calculator buttons and test with events

--- calculator.js
+++ calculator.js
@@ -1,2 +1,17 @@
-let calculatorScreen = $('.calculator-screen');
-calculatorScreen.text('Test');
+let Calculator = () => {
+    let accumulator = 0;
+
+    let add = (num1, num2) => num1 + num2;
+    let subtract = (num1, num2) => num1 - num2;
+    let multiply = (num1, num2) => num1 * num2;
+    let divide = (num1, num2) => {
+        if ( num2 === 0 ) return undefined;
+        return num1 / num2;
+     }
+    
+    let keys = $('.key').on({
+        'click' : (event) => console.log("You pressed", event.target.value)
+    })
+}
+
+Calculator();
