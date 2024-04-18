2efec46e416ed0a0e45f1db0cd64739b6495b5ed
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2024-04-17 23:04:04 -0400

    Make screen readonly and fix issue with trailing zero on new input

--- calculator.js
+++ calculator.js
@@ -1,5 +1,8 @@
 let Calculator = () => {
+    const maximumDigitsOnScreen = 7;
     let accumulator = 0;
+    let calculatorScreen = $('.calculator-screen');
+    calculatorScreen.val(0);
 
     let add = (num1, num2) => num1 + num2;
     let subtract = (num1, num2) => num1 - num2;
@@ -8,10 +11,39 @@ let Calculator = () => {
         if ( num2 === 0 ) return undefined;
         return num1 / num2;
      }
+    let parseInputFromCalculatorScreen = () => parseInt(calculatorScreen.val());
+    let updateCalculatorScreen = () => calculatorScreen.val(accumulator);
     
-    let keys = $('.key').on({
-        'click' : (event) => console.log("You pressed", event.target.value)
-    })
+    let operators = $('[data-type="operator"]').on({
+        'click' : (event) => {
+            let operation = event.target.value;
+
+            switch(operation) {
+                case 'ce' :
+                    accumulator = 0;
+                    calculatorScreen.val(0);
+                    break;
+                case '+' :
+                    accumulator = add( accumulator, parseInputFromCalculatorScreen() ); 
+                    break;
+                case '=' :
+                    updateCalculatorScreen();
+                default:
+                    break;
+            }
+        }
+    });
+
+    let numbers = $('[data-type="number"]').on({
+        'click': (event) => {
+            let currentInputOnScreen = calculatorScreen.val() !== '0' ? calculatorScreen.val() : '';
+           
+            if (currentInputOnScreen.length < maximumDigitsOnScreen) {
+                calculatorScreen.val( currentInputOnScreen + event.target.value);
+            }
+                
+        }
+    });
 }
 
 Calculator();
