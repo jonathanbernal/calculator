1567b26730201ebc362f9be34ffb3873cf592364
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2024-04-29 00:06:46 -0400

    Add operator table and operate function

--- calculator.js
+++ calculator.js
@@ -1,10 +1,15 @@
+import Stack from './Stack';
+
 let Calculator = () => {
     const maximumDigitsOnScreen = 7;
     let accumulator = 0;
     let wasOperatorPressed = false;
     let lastOperatorPressed = null;
     let calculatorScreen = $('.calculator-screen');
-    calculatorScreen.val(0);
+
+    // Use two stacks for the parsing algorithm
+    let valueStack = Stack();
+    let operatorStack = Stack();
 
     let add = (num1, num2) => num1 + num2;
     let subtract = (num1, num2) => num1 - num2;
@@ -13,79 +18,88 @@ let Calculator = () => {
         if ( num2 === 0 ) return undefined;
         return num1 / num2;
     }
-    let parseInputFromCalculatorScreen = () => parseInt(calculatorScreen.val());
-    let updateCalculatorScreen = () => calculatorScreen.val(accumulator);
-    
-    let operators = $('[data-type="operator"]').on({
-        'click' : (event) => {
-            let operator = event.target.value;
-            let valueOnScreen = parseInputFromCalculatorScreen();
-            if ( operator !== '=' ) {
-                lastOperatorPressed = operator;
-            }
 
-            switch(operator) {
-                case 'ce' :
-                    wasOperatorPressed = false;
-                    accumulator = 0;
-                    calculatorScreen.val('0');
+    let operatorTable = {
+        '+' : add,
+        '-' : subtract,
+        '*' : multiply,
+        '/' : divide,
+    }
+
+    // input variables
+    let firstOperand = null, operator = null, secondOperand = null;
+    let userInput = '';
+
+    /**
+     * Performs the assigned operation on the table
+     * @param {float} firstOperand 
+     * @param {float} secondOperand 
+     * @param {char} operator 
+     * @returns 
+     */
+    let operate = ( firstOperand, secondOperand, operator ) => {
+        let allowedOperators = Object.keys( operatorTable );
+        let isOperatorInAllowedOperatorList = allowedOperators.includes( operator );
+
+        if ( isOperatorInAllowedOperatorList ) return operatorTable[operator]( firstOperand, secondOperand );
+        return undefined;
+    }
+    
+    /**
+     * Gets the input from the screen as a floating-point number.
+     * @returns the float representation of the current input on the calculator screen
+     */
+    let parseInputFromCalculatorScreen = () => parseFloat(calculatorScreen.val());
+
+    /**
+     * Updates the value on the screen based on the accumulator's value
+     * @returns the value on the calculator screen
+     */
+    let updateCalculatorScreen = () => calculatorScreen.val(accumulator);
+    let clearScreen = () => calculatorScreen.val('');
+
+    let numberEvents = $('[data-type="number"]').on({
+        'click' : (event) => {
+            let newInputReceived = event.target.value;
+
+            userInput += newInputReceived;
+
+            calculatorScreen.val( calculatorScreen.val() + newInputReceived );
+        }
+    });
+
+    let operatorEvents = $('[data-type="operator"]').on({
+        'click' : (event) => {
+            let operatorPressed = event.target.value;
+
+            switch ( operatorPressed ) {
+                case 'ce':
+                    userInput = '';
+                    clearScreen();
                     break;
-                case '+' :
-                    if ( !wasOperatorPressed && valueOnScreen !== '0' ) {
-                        accumulator = add(accumulator, valueOnScreen);
-                        updateCalculatorScreen();
-                        wasOperatorPressed = !wasOperatorPressed;
-                    } else if ( wasOperatorPressed ) {
-                        accumulator = add(accumulator, valueOnScreen);
-                        calculatorScreen.val(accumulator);
-                    }
+                case '=':
+                    let operationToPerform = userInput.split(' ');
+                    firstOperand = parseFloat(operationToPerform[0]);
+                    operator = operationToPerform[1];
+                    secondOperand = parseFloat(operationToPerform[2]);
+                    let result = operate(firstOperand, secondOperand, operator);
+                    calculatorScreen.val(result)
                     break;
+                case '+':
                 case '-':
-                    break;
                 case '*':
-                    if ( !wasOperatorPressed && valueOnScreen !== '0' ) {
-                        accumulator = multiply(accumulator, valueOnScreen);
-                        updateCalculatorScreen();
-                        wasOperatorPressed = !wasOperatorPressed;
-                    } else if ( wasOperatorPressed ) {
-                        accumulator = multiply(accumulator, valueOnScreen);
-                        updateCalculatorScreen();
-                    }
-                    break;
                 case '/':
+                    userInput += ' ' + operatorPressed + ' ';
                     break;
-                case '=' :
-                    // check for last operation button pressed and perform the last operation before displaying the final result
-                    switch ( lastOperatorPressed ) {
-                        case '+':
-                            accumulator = add(accumulator, valueOnScreen);
-                            calculatorScreen.val(accumulator);
-                            break;
-                        case '-':
-                            accumulator = subtract(accumulator, valueOnScreen);
-                            calculatorScreen.val(accumulator);
-                            break;
-                    }
                 default:
                     break;
             }
+
+            
         }
     });
 
-    let numbers = $('[data-type="number"]').on({
-        'click': (event) => {
-            if ( wasOperatorPressed ) {
-                wasOperatorPressed = !wasOperatorPressed;
-                calculatorScreen.val('');
-            }
 
-            let currentInputOnScreen = calculatorScreen.val() !== '0' ? calculatorScreen.val() : '';
-           
-            if (currentInputOnScreen.length < maximumDigitsOnScreen) {
-                calculatorScreen.val( currentInputOnScreen + event.target.value);
-            }
-        }
-    });
 }
 
 Calculator();
